855688999970015b58b9886dfc5b0953405e066e
Jev Kuznetsov <jev.kuznetsov@gmail.com>
2021-10-10 12:45:18 +0200

    update readme and cli

--- README.md
+++ README.md
@@ -1,15 +1,30 @@
 
-=================
 # Android Debloater
-=================
+
 
 
 Android debloater with ADB automation
 
+`adbloater` is a simple command line tool that automates package removal with `adb`.
 
 
-Features
---------
+## Installation
 
-* TODO
+`pip install -e .`
 
+
+
+## Usage
+
+
+
+For list of commands see
+
+`adbloater --help`
+
+
+## Development
+
+1. create virtual environment `make venv`
+2. activate `source venv/bin/activate`
+3. install in editable mode `make install`
--- adbloater/adbloater.py
+++ adbloater/adbloater.py
@@ -1,6 +1,29 @@
 """Console script for adbloater."""
+
+import os
 import sys
 import click
+
+
+class bcolors:
+    """terminal color definitions"""
+
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
+
+def shell_cmd(cmd):
+    """run shell command, echo to screen"""
+
+    print(bcolors.OKCYAN + ">" + cmd + bcolors.ENDC)
+    os.system(cmd)
 
 
 @click.command()
