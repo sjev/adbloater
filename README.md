50ae1dc40db066b2137fcbe4610e0df1aace91f9
Jev Kuznetsov <jev.kuznetsov@gmail.com>
2021-10-10 12:34:23 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+
+=================
+# Android Debloater
+=================
+
+
+Android debloater with ADB automation
+
+
+
+Features
+--------
+
+* TODO
+
---
+++ adbloater/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for Android Debloater."""
+
+__author__ = """Jev Kuznetsov"""
+__email__ = "jev.kuznetsov@gmail.com"
+__version__ = "0.1.0"
